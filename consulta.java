005fc5835c09a691eba2996968bdf48b9c28dcb1
Daniel Montero <32424377+Danimonarc@users.noreply.github.com>
2018-11-29 12:51:19 +0100

    Add files via upload

---
+++ consulta.java
@@ -0,0 +1,132 @@
+package Interfaz;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import prototype.Beneficiario;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.Vector;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JToggleButton;
+import javax.swing.JScrollBar;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+
+public class consulta extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField entrada;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					consulta frame = new consulta();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public consulta() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		entrada = new JTextField();
+		panel.add(entrada);
+		entrada.setColumns(10);
+		
+		JButton botonAceptar = new JButton("Buscar");
+		panel.add(botonAceptar);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		
+		JButton modificar = new JButton("Modificar");
+		panel_1.add(modificar);
+		
+		JButton eliminar = new JButton("Eliminar\r\n");
+		panel_1.add(eliminar);
+		
+		JButton btnVolver = new JButton("Volver");
+		panel_1.add(btnVolver);
+		String[] colums = {"Nombre", "Fecha de nacimiento", "Localidad"};
+		
+		
+		JTable table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Nombre", "Edad", "ID"
+			}
+		));
+		table.getColumnModel().getColumn(1).setPreferredWidth(53);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setCellSelectionEnabled(true);
+		DefaultTableModel modelo = (DefaultTableModel) table.getModel();
+		
+		botonAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				int ID = Integer.parseInt(entrada.getText());
+				Beneficiario ben = new Beneficiario(ID);
+				if(!ben.equals(null)) 
+				{
+					Object[] bena = {ben.getName(),ben.getBirthDate(), ben.getId()};
+					
+					modelo.addRow(bena);
+				}
+				
+				
+			}});
+		modificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				int id;
+				id =Integer.parseInt(table.getValueAt(table.getSelectedRow(), 2).toString());
+				//new vistaNiño(new Beneficiario(id)).setVisible(true);
+				
+				
+				
+			}});
+		
+		
+		
+		contentPane.add(table, BorderLayout.CENTER);
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();}});
+	}
+
+}
